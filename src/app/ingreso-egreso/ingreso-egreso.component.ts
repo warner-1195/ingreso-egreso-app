b0d6ebe1bc473c592ff57d567a351074f653be3d
Warner Sanchez <chocobarber0895@gmail.com>
2022-09-12 16:45:10 -0600

    primer comit - template listo

---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styleUrls: []
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
